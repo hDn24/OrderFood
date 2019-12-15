1619c2a040d85a8361bc3bdd1cae02957b2d9171
hDn <hodangnhan24@gmail.com>
2019-12-15 16:45:08 +0700

    Upload

---
+++ app/src/main/java/com/hdn/orderfood/DTO/ChiTietGoiMonDTO.java
@@ -0,0 +1,31 @@
+package com.hdn.orderfood.DTO;
+
+public class ChiTietGoiMonDTO {
+    int MaMonAn,MaGoiMon,SoLuong;
+
+    public int getMaMonAn() {
+        return MaMonAn;
+    }
+
+    public void setMaMonAn(int maMonAn) {
+        MaMonAn = maMonAn;
+    }
+
+    public int getMaGoiMon() {
+        return MaGoiMon;
+    }
+
+    public void setMaGoiMon(int maGoiMon) {
+        MaGoiMon = maGoiMon;
+    }
+
+    public int getSoLuong() {
+        return SoLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        SoLuong = soLuong;
+    }
+
+
+}
